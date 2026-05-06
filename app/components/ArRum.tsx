48062ebfeb9135f6c9d323cd8966e97d53f2553c
redheet <naufalzidane32@gmail.com>
2026-05-07 01:06:09 +0700

    change span text

--- app/components/ArRum.tsx
+++ app/components/ArRum.tsx
@@ -7,9 +7,9 @@ function ArRum() {
       <div className="bg-[url('/images/anNur/bg.png')] rounded-t-[1.25rem] bg-center bg-no-repeat bg-cover p-12 flex flex-col items-center text-center gap-8 -mt-8 z-20 relative font-light">
         <RevealWrapper duration={1500} origin="top">
           <h1 className="italic text-4xl flex gap-4 text-[#A99C86]">
-            <span>A</span>
+            <span>R</span>
             <span>&</span>
-            <span>M</span>
+            <span>S</span>
           </h1>
         </RevealWrapper>
         <RevealWrapper duration={1500} origin="top">
