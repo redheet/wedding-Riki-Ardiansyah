8fd02188b61d5e25e22593ac77ab49cb9e08142d
redheet <naufalzidane32@gmail.com>
2026-05-07 01:03:26 +0700

    bew config resolve

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.symlinks = true;
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "firebase/app": path.resolve(
+        __dirname,
+        "node_modules/firebase/app/dist/index.cjs.js"
+      ),
+      "firebase/firestore": path.resolve(
+        __dirname,
+        "node_modules/firebase/firestore/dist/index.cjs.js"
+      ),
+    };
+    return config;
+  },
+};
 
 module.exports = nextConfig;
