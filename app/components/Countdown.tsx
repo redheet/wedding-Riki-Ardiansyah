ed6d6fa5b47a546a181211298e3526e80d6c5c2b
redheet <naufalzidane32@gmail.com>
2026-05-07 01:06:48 +0700

    change bg img & time

--- app/components/Countdown.tsx
+++ app/components/Countdown.tsx
@@ -29,7 +29,7 @@ const CountdownComp: React.FC = () => {
 
   return (
     <section id="countdown">
-      <div className="min-h-screen bg-[url('/images/countdown/2.jpg')] bg-cover bg-[44.5%] text-white relative py-28 flex items-end">
+      <div className="min-h-screen bg-[url('/images/countdown/1.webp')] bg-cover bg-[44.5%] text-white relative py-28 flex items-end">
         <div className="absolute inset-0 bg-black/30" />
         <div className="relative py-8 px-10 w-full">
           <div
@@ -39,16 +39,16 @@ const CountdownComp: React.FC = () => {
           <div className="relative z-20">
             <RevealWrapper duration={1500} origin="right">
               <p className="text-sm font-light">WEDDING INVITATION</p>
-              <h1 className="text-3xl font-light mt-2 mb-4">Maulana & Aura</h1>
+              <h1 className="text-3xl font-light mt-2 mb-4">Riki Ardiansyah & Silvia</h1>
               <div className="grid grid-cols-2 border-y border-y-white text-center text-[0.75rem] font-light">
                 <p className="leading-[1.85] py-2 px-2 border-r border-r-white flex flex-col gap-2">
                   <span>SAVE THE DATE</span>
-                  <span>Minggu, 2 Februari 2025</span>
+                  <span>Sabtu, 16 Mei 2026</span>
                 </p>
                 <p className="py-2 px-2 flex flex-col gap-2">
                   <span>COUNTDOWN</span>
                   <Countdown
-                    date={new Date("2025-02-02")}
+                    date={new Date("2026-05-16")}
                     renderer={renderer}
                   />
                 </p>
